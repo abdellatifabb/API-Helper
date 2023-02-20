e7df9a740bb238337f3dd5dd9f30dafd77b34bc6
Abdellatif ER-RAZZOUQI <abdellatif.er-razzouqi@edu.uca.ma>
2023-02-20 13:39:27 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser')
+const request = require('request');
+
+const app = express();
+const router = express.Router();
+
+var jsonParser = bodyParser.json()
+
+router.get('/', (req, res) => {
+    res.json({
+        'hello': 'Index page'
+    });
+});
+
+router.get('/another', (req, res) => {
+    res.json({
+        'hello': 'Another page'
+    });
+});
+
+router.post('/uploadfile', (req, res) => {
+    var options = {
+      'method': 'POST',
+      'url': 'https://upload.api.synthesia.io/v2/scriptAudio',
+      'headers': {
+        'Authorization': '35b071223a6953d5ac60714f8a386de3',
+        'Content-Type': 'audio/mpeg'
+      },
+      body: req.body.file
+    
+    };
+    request(options, function (error, response) {
+    
+      if (error) throw new Error(error);
+      res.send(response.body)
+      console.log(response.body);
+    });
+    
+})
+
+app.use('/.netlify/functions/api', router);
+
+module.exports.handler = serverless(app);
